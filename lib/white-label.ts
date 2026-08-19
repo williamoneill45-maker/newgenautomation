a115a19181a56115fec5f58b97b648a91bb09069
Admin <williamoneill45@gmail.com>
2026-08-19 19:44:46 +1200

    Fix demo contact details in templates

--- lib/white-label.ts
+++ lib/white-label.ts
@@ -1,6 +1,8 @@
 export const whiteLabelFirmName = "New Gen Automation";
 export const whiteLabelFirmNameUpper = "NEW GEN AUTOMATION";
-export const whiteLabelEmail = "hello@newgenautomation.nz";
+export const whiteLabelEmail = "will@newgenbusiness.nz";
+export const whiteLabelLandline = "(09) 235 5523";
+export const whiteLabelMobile = "027 230 5006";
 export const whiteLabelLocation = "Auckland 1010";
 export const whiteLabelServiceAddress = [
   `c/o ${whiteLabelFirmName}`,
